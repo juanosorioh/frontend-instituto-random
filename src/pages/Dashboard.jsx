7c9429899184738a20e72474e1dd3cd158191521
juanosorioh <juangabrielosorioheredia@gmail.com>
2022-11-29 03:07:30 -0300

    .

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,54 +3,51 @@ import { Link } from "react-router-dom";
 import Table from "./Table";
 
 const Dashboard = () => {
+  const listStyle = {
+    margin: "0px 0px 10px 0px"
+  }
   return (
     <div>
       <div className="container-fluid">
         <div className="row">
-          <nav
+          <nav 
             id="sidebarMenu"
             className="col-md-3 col-lg-3 d-md-block bg-light sidebar collapse"
           >
-            <div className="position-sticky pt-3">
-              <ul className="nav flex-column">
-                <li className="nav-item">
+            <div className="position-sticky pt-3" style={{display: "flex", textAlign: "center", justifyContent: "center"}}>
+              <ul className="nav flex-column" >
+                <li className="nav-item" style={listStyle}>
                   <Link className="nav-link active" aria-current="page" to={"/formAnuncios"}>
                     <span data-feather=""></span>
                     Agregar Anuncio
                   </Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" style={listStyle}>
                   <Link className="nav-link" to={"/formMaterias"}>
                     <span data-feather=""></span>
                     Agregar Materia
                   </Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" style={listStyle}>
                   <Link className="nav-link" to={"/formCarreras"}>
                     <span data-feather=""></span>
                     Agregar Carrera
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/"}>
+                <li className="nav-item" style={listStyle}>
+                  <Link className="nav-link" to={"/adminPersona"}>
                     <span data-feather=""></span>
                     Administrar Persona
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/"}>
-                    <span data-feather=""></span>
-                    Administrar anuncios
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/"}>
+                <li className="nav-item" style={listStyle}>
+                  <Link className="nav-link" to={"/adminMateria"}>
                     <span data-feather=""></span>
                     Administrar Materias
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/"}>
+                <li className="nav-item" style={listStyle}>
+                  <Link className="nav-link" to={"/adminCarrera"}>
                     <span data-feather=""></span>
                     Administrar Carreras
                   </Link>
